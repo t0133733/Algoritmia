338525ee6a095d9671c36bfa18268b411e6878b7
t0133733 <nuno.campos.t0133733@edu.atec.pt>
2025-10-29 16:10:43 +0000

    Ficha01

--- FichasPraticas/src/Ficha01/Ex07.java
+++ FichasPraticas/src/Ficha01/Ex07.java
@@ -1,4 +1,32 @@
 package Ficha01;
 
+import java.util.Scanner;
+
 public class Ex07 {
-}
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double preco1, preco2, preco3, desconto;
+
+                System.out.print("Preço 1: ");
+
+                preco1 = input.nextDouble();
+
+                System.out.print("Preço 2: ");
+
+                preco2 = input.nextDouble();
+
+                System.out.print("Preço 3: ");
+
+                preco3 = input.nextDouble();
+
+                desconto = (preco1 + preco2 + preco3) * .10;
+
+                System.out.println("Desconto 10%: " + desconto );
+
+
+
+        System.out.println("Total: " + (preco1 + preco2 + preco3 - desconto));
+    }
+    }
---
+++ FichasPraticas/src/Ficha01/Ex08.java
@@ -0,0 +1,67 @@
+package Ficha01;
+
+import java.util.Scanner;
+
+public class Ex08 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int musica1, musica2, musica3, musica4,musica5, segundos1, minutos1, segundos2, minutos2, segundos3, minutos3, segundos4, minutos4, segundos5, minutos5, horas;
+
+        System.out.print("Segundos da música 1: ");
+
+        segundos1 = input.nextInt();
+
+        System.out.print("Minutos da música 1: ");
+
+        minutos1 = input.nextInt();
+
+        System.out.print("Segundos da música 2: ");
+
+        segundos2 = input.nextInt();
+
+        System.out.print("Minutos da música 2: ");
+
+        minutos2 = input.nextInt();
+
+        System.out.print("Segundos da música 3: ");
+
+        segundos3 = input.nextInt();
+
+        System.out.print("Minutos da música 3: ");
+
+        minutos3 = input.nextInt();
+
+        System.out.print("Segundos da música 4: ");
+
+        segundos4 = input.nextInt();
+
+        System.out.print("Minutos da música 4: ");
+
+        minutos4 = input.nextInt();
+
+        System.out.print("Segundos da música 5: ");
+
+        segundos5 = input.nextInt();
+
+        System.out.print("Minutos da música 5: ");
+
+        minutos5 = input.nextInt();
+
+
+
+
+
+
+
+
+        System.out.println("Total do Álbum: ");
+
+
+
+
+
+
+
+}
